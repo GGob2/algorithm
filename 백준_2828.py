2b5b109bd6cc78863344dab5439a854ad24d7136
GGOB2 <rkdaudwh13@naver.com>
2021-11-16 21:04:32 +0900

    백준_2828_수정

--- 백준_2828.py
+++ 백준_2828.py
@@ -1,7 +1,6 @@
 a, b = map(int, input().split())
 c = int(input())
 basket = []
-result = 0
 
 for i in range(c):
     basket.append(int(input()))
@@ -10,10 +9,10 @@ m = 0
 end = b
 start = 1
 
-for _ in range(len(basket)):
+for _ in range(c):
     if end >= basket[_] and start <= basket[_]:
         continue
-    elif end <= basket[_]:
+    elif end < basket[_]:
         m += basket[_] - end
         end = basket[_]
         start = basket[_] - (b - 1)
@@ -21,6 +20,6 @@ for _ in range(len(basket)):
     elif start > basket[_]:
         m += start - basket[_]
         start = basket[_]
-        end = basket[_] - (b - 1)
+        end = basket[_] + (b - 1)
 
 print(m)
