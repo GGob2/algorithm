d3036a7f5cc376b9d9c4830fb1331ab14680b1f5
GGOB2 <rkdaudwh13@naver.com>
2021-11-25 23:25:26 +0900

    11/25 백준

--- 백준_1343_unsolvec.py
+++ 백준_1343_unsolvec.py
@@ -1,46 +1,59 @@
 board = list(input())
 result = list()
 result_v2 = list()
+is_point = False
 error = False
-trans = 0
 
-for i in board:
-    if i == "." and trans != 0:
-        result.append(trans)
-        trans = 0
-    elif i == ".":
-        continue
-    else:
-        trans += 1
-        
+X_len = 0
+point_len = 0
 
-for k in result:
-    if k % 2 != 0:
-         error = True
-
-    if k & 4 == 0:
-        element = "AAAA" * (len(i) // 4)
-        result_v2.append(element)
-        element = ""
+for i in range(len(board)):
+    if i == len(board) -1 and board[i] != ".":
+        result.append(X_len+1) 
     
-    if k % 4 == 2:
-        element = "AAAA" * ((len(i)-2) // 4)
-        element += "BB"
-        result_v2.append(element)
-        element = ""
+    elif i == len(board) -1 and board[i] == ".":
+        result.append(X_len+1) 
+     
+    elif board[i] != ".":
+        is_point = False
+        X_len += 1
 
-    if k == 2:
-        element = "BB"
-        result_v2.append(element)
-        element = ""
+    elif board[i] == "." and is_point == False:
+        result.append([(i - X_len), X_len])
+        X_len = 0
+        is_point = True
+
+    else:
+        continue
 
 
-if error == True:
-    print(-1)
-else:
-    for j in result_v2:
-        if len(result_v2) == j:
-            print(j)
-        else:
-            print(j, end=".")
+# for k in result:
+#     if k % 2 != 0:
+#          error = True
+
+#     if k & 4 == 0:
+#         element = "AAAA" * (len(i) // 4)
+#         result_v2.append(element)
+#         element = ""
+    
+#     if k % 4 == 2:
+#         element = "AAAA" * ((len(i)-2) // 4)
+#         element += "BB"
+#         result_v2.append(element)
+#         element = ""
+
+#     if k == 2:
+#         element = "BB"
+#         result_v2.append(element)
+#         element = ""
+
+
+# if error == True:
+#     print(-1)
+# else:
+#     for j in result_v2:
+#         if len(result_v2) == j:
+#             print(j)
+#         else:
+#             print(j, end=".")
         
