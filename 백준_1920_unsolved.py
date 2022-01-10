5b89ac1594bc733d9b0357d27994015ffb4f50cd
GGOB2 <rkdaudwh13@naver.com>
2022-01-10 22:54:23 +0900

    1/10 백준

--- 백준_1920_unsolved.py
+++ 백준_1920_unsolved.py
@@ -1,19 +1,28 @@
 import sys
 
 n = int(sys.stdin.readline())
-
 num_list = list(map(int, sys.stdin.readline().split()))
+num_list.sort()
 
 m = int(input())
-
 find_list = list(map(int, sys.stdin.readline().split()))
 
-result = []
+def binary_search(target_num):
+    left = 0
+    right = n - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if num_list[mid] == target_num:
+            return True
+        
+        if target_num < num_list[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
 
 for i in find_list:
-    if num_list.count(i) == 0:
-        result.append("0")
+    if binary_search(i):
+        print(1)
     else:
-        result.append("1")
-
-print("\n".join(result[:]))
+        print(0)
