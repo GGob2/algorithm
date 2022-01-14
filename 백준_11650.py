afa91d84819f437af4a666846f7522f39b5ac030
GGOB2 <rkdaudwh13@naver.com>
2022-01-14 23:24:15 +0900

    1/14 백준

---
+++ 백준_11650.py
@@ -0,0 +1,14 @@
+import sys
+n = int(sys.stdin.readline())
+result = []
+
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    result.append([a, b])
+
+result.sort(key=lambda x: (x[0], x[1]))
+
+
+
+for j in range(n):
+    print(result[j][0], result[j][1])
