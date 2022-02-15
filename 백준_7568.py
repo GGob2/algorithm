49ca6f2ebb406c3d49fda99b9e5b9236874fd0c7
GGOB2 <rkdaudwh13@naver.com>
2022-02-15 22:42:34 +0900

    2/15 백준

--- 백준_7568.py
+++ 백준_7568.py
@@ -1,10 +1,17 @@
 n = int(input())
 
 people = []
-result = []
+
 for i in range(n):
     a, b = map(int, input().split())
-    people.appned([a, b])
+    people.append([a, b])
+
+for j in people:
+    rank = 1
+    for k in people:
+        if j[0] < k[0] and j[1] < k[1]:
+            rank += 1
+    print(rank, end=" ")
 
 
     
