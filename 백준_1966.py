6468261ce3772e7f9fda12c4decf08c5694ec8f1
GGOB2 <rkdaudwh13@naver.com>
2022-02-17 01:47:42 +0900

    2/16 백준

--- 백준_1966.py
+++ 백준_1966.py
@@ -1,13 +1,32 @@
-from re import L
+from tabnanny import check
 
 
 n = int(input())
 
 for i in range(n):
     a, b = map(int, input().split())
-    c = map(int, input().split())
+    num_list = list(map(int, input().split()))
+    rank = 0
+    check_list = [0 for i in range(a)]
+    check_list[b] = 1
 
-    for j in range(a):
+    while True:
+        if  num_list[0] == max(num_list):
+            rank += 1
+
+            if check_list[0] != 1:
+                del num_list[0]
+                del check_list[0]
+            else:
+                print(rank)
+                break
+        else:
+            num_list.append(num_list[0])
+            check_list.append(check_list[0])
+            del num_list[0]
+            del check_list[0]
+
+    
         
 
         
