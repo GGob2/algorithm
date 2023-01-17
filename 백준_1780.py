68e75017018a83baa1e80ed9309dac1c84e70d66
GGOb2 <rkdaudwh13@naver.com>
2023-01-17 10:13:05 +0900

    1/16 백준

--- 백준_1780.py
+++ 백준_1780.py
@@ -1,16 +1,37 @@
+# <https://www.acmicpc.net/problem/1780>
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
+num_list = [list(map(int, input().split())) for _ in range(n)]
 
-num_list = []
+zero_paper = 0
+one_paper = 0
+minus_one_paper = 0
 
-for i in range(n):
-    num = input().split()
-    num_list.append(num)
+def dfs(r, c, n):
+    global zero_paper, one_paper, minus_one_paper
 
-def check_paper(list):
-    for _ in range(0, n, n/3):
-        
+    num = num_list[r][c]
+    
+    for i in range(r, r+n):
+        for j in range(c, c+n):
+            if num_list[i][j] != num:
+                for k in range(3):
+                    for l in range(3):
+                        dfs(r+k*n // 3, c+l*n // 3, n // 3)
+                return
 
 
+    if num == 0:
+        zero_paper += 1
+    elif num == 1:
+        one_paper += 1
+    else:
+        minus_one_paper += 1
+
+dfs(0,0,n)
+print(minus_one_paper)
+print(zero_paper)
+print(one_paper)
