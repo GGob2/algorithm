cc2d68447715c70f89c6bee14c302c00587e3211
GGOb2 <rkdaudwh13@naver.com>
2023-04-18 10:49:08 +0900

    백준 0418

--- 백준_15650.py
+++ 백준_15650.py
@@ -4,17 +4,19 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 result = []
 
-def back():				# 백트래킹을 위한 재귀 함수
+def back(num):				# 백트래킹을 위한 재귀 함수
+    sorted_list = sorted(result)
+    
     if len(result) == m:	# 길이가 m이면 출력
         print(' '.join(map(str, result))) 
         return
     
-    for i in range(1, n+1):	# 사용 가능한 자연수 범위 내에서
+    for i in range(num, n+1):	# 사용 가능한 자연수 범위 내에서
         if i in result:			# 배열에 문자가 있으면 넘어감
             continue
        
         result.append(i)		# 결과에 넣고 
-        back()					# 재귀가 끝나면
+        back(i+1)				# 재귀가 끝나면
         result.pop()			# 다시 빼줌
 
-back()
+back(1)
--- 백준_15650_v2.py
+++ 백준_15650_v2.py
@@ -5,16 +5,19 @@ n, m = map(int, input().split())
 result = []
 
 def back():				# 백트래킹을 위한 재귀 함수
+    sorted_list = sorted(result)
+    
     if len(result) == m:	# 길이가 m이면 출력
-        print(' '.join(map(str, result))) 
-        return
+        if result == sorted_list:
+            print(' '.join(map(str, result))) 
+            return
     
     for i in range(1, n+1):	# 사용 가능한 자연수 범위 내에서
         if i in result:			# 배열에 문자가 있으면 넘어감
             continue
        
         result.append(i)		# 결과에 넣고 
-        back()					# 재귀가 끝나면
+        back()	    			# 재귀가 끝나면
         result.pop()			# 다시 빼줌
 
 back()
