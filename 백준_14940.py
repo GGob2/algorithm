c135effde283e452cfbf5bd192ec14619fa27c78
GGOb2 <rkdaudwh13@naver.com>
2023-08-29 17:44:52 +0900

    백준 0829

--- 백준_14940.py
+++ 백준_14940.py
@@ -1,36 +1,45 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
 land = []
-visit = []
+queue = deque()
+visit = [[False] * m for _ in range(n)]
+result = [[-1] * m for _ in range(n)]
+
 for i in range(n):
     row = list(map(int, input().split()))
     land.append(row)
+    
     if 2 in row:
-        visit.append([i, row.index(2)])
+        queue.append([i, row.index(2)])  # 목표 지점 탐색
+        visit[i][row.index(2)] = True
+        result[i][row.index(2)] = 0
+    
+    for j in range(m):
+        if row[j] == 0:
+            result[i][j] = 0
 
 dx = [-1, 1, 0, 0]
-dy = [0, 0, 1, -1]
-cnt = 0
+dy = [0, 0, -1, 1]
 
-while(visit):
-    x, y = visit.pop()
-    
-    if land[x][y] == 0:
-        continue
-
-    land[x][y] = cnt
-    cnt += 1
+while(queue):
+    y, x = queue.popleft()     
 
     for _ in range(4):
-        ax = x+dx[_]
         ay = y+dy[_]
-        if [ax, ay] not in visit:
-            if 0 <= ax <= n and 0 <= ay <= m:
-                visit.append([ax, ay])
-                
+        ax = x+dx[_]
+        
+        if 0 <= ax < m and 0 <= ay < n: 
+            if not visit[ay][ax] and land[ay][ax] == 1:        
+                queue.append([ay, ax])
+                visit[ay][ax] = True
+                result[ay][ax] = result[y][x] + 1
 
-
+for i in result:
+    for j in i:
+        print(j, end =' ')
+    print()
